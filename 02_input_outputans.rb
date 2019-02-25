1d84e7cb302b52e89c7ba2d6693d77027290e10b
lomaslove <lomaslove132@gmail.com>
2019-02-26 09:25:22 +1000

    README

---
+++ 02_input_outputans.rb
@@ -0,0 +1,7 @@
+puts "what is your name"
+user_entry=gets.chomp
+puts " hi #{user_entry} how old are you?"
+user_entry1=gets.chomp
+puts" wow you are #{user_entry1} years old, 
+congralutions!"
+puts "hi #{user_entry} in the next #{100.to_i-user_entry1.to_i} years you will be 100"
